9fe974d2701dad62876aa974e65f554a14cf21a8
shaywacker <shay2442@yahoo.com>
2022-01-06 17:30:53 -0600

    commit tree fee3df477186efd3419867d69c465c829bca4998

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,28 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery_name = Bakery.find(params[:id])
+    bakery_name.update(
+      name: params[:name]
+    )
+    bakery_name.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
 end
+
